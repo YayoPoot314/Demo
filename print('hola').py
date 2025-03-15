4a47c65740070d79975bb90ea48d872298eb771b
GUSTAVO YAHIR POOT RODRÍGUEZ <YAHIRPOOT211@GMAIL.COM>
2025-03-15 17:29:28 -0600

    respasldo

--- print('hola').py
+++ print('hola').py
@@ -1,1 +1,5 @@
-print('hola')
+print('hola')}
+
+setDocumentLocator(addsitedir(add(assertas
+                                  data()setDocumentLocator(sendall(setDocumentLocator(assertdas
+                                                                                      ), flags])), fragment2, width), known_paths=None))
